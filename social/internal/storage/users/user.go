bf8d04cbf7d6aa21e48b6312a6f9dde397671742
Vladimir <site116@gmail.com>
2019-10-24 15:38:42 +0300

    fix

--- social/internal/storage/users/user.go
+++ social/internal/storage/users/user.go
@@ -21,7 +21,7 @@ type UserDB struct {
 	Interests   sql.NullString
 	DateCreated time.Time `db:"date_created"`
 	DateModify  time.Time `db:"date_modify"`
-	Age int32
+	Age         int32
 }
 
 type UserRole struct {
