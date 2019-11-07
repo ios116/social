9bda38fa20644396ba4fa3f062dc94ecb20b128f
Владимир <site116@gmail.com>
2019-11-08 00:51:53 +0300

    fix

--- mysql-conf/slave/setup.sql
+++ mysql-conf/slave/setup.sql
@@ -1,25 +1,5 @@
--- BEGIN;
--- --
--- -- Create entities User
--- --
--- CREATE TABLE IF NOT EXISTS `users`
--- (
---     `id`           serial PRIMARY KEY,
---     `login`        varchar(255) NOT NULL UNIQUE check ( login <> '' ),
---     `password`     varchar(255) NOT NULL CHECK (password <> ''),
---     `email`        varchar(255) NOT NULL CHECK (email <> ''),
---     `age`          int NOT NULL default 30,
---     `city`         varchar(255),
---     `gender`       varchar(255),
---     `interests`    text,
---     `first_name`   varchar(255),
---     `last_name`    varchar(255),
---     `date_created` datetime     NOT NULL default NOW(),
---     `date_modify`  datetime     NOT NULL default NOW(),
---     check (gender in ('male', 'female', 'Unknown')),
---     KEY `f` (`first_name`),
---     KEY `l` (`last_name`)
--- );
--- COMMIT;
 CHANGE MASTER TO MASTER_HOST = 'master', MASTER_PORT = 3306,  MASTER_USER = 'slave_user', MASTER_PASSWORD = 'qwerty', MASTER_AUTO_POSITION = 1;
 START SLAVE;
+INSTALL PLUGIN rpl_semi_sync_slave SONAME 'semisync_slave.so';
+STOP SLAVE IO_THREAD;
+START SLAVE IO_THREAD;
